caa3d275bcce7a4a83474a69bcf1c0f05a8efd8b
Primevix <jumerle19@gmail.com>
2022-11-09 19:42:56 -0300

    Update bignumber-card-icon.js

--- bignumber-card-icon.js
+++ bignumber-card-icon.js
@@ -126,7 +126,10 @@ class BigNumberCardIcon extends HTMLElement {
       if (config.hideunit==true) 
         { root.getElementById("value").textContent = `${value}`; }
       else 
-        { root.getElementById("value").innerHTML = `${showIcon}${value}<small>${measurement}</small>`; }
+        if (config.showicon==true)
+          { root.getElementById("value").innerHTML = `${showIcon}${value}<small>${measurement}</small>`; }
+        else
+          { root.getElementById("value").innerHTML = `${value}<small>${measurement}</small>`; }
       if (this.isNoneConfig){
         if (isNaN(value)) {
           if (config.noneString) {
